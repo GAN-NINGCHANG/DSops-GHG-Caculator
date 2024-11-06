8553129c13eea39c1a2aa6531b074163f1a6648f
GAN NINGCHANG <e1351391@u.nus.edu>
2024-11-06 18:36:49 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from pages import home, page_1, page_2, page_3
 
+st.set_page_config(layout="wide")  # 设置页面为宽屏布局
+
 # 初始化页面状态
 if 'current_page' not in st.session_state:
     st.session_state.current_page = 0  # 0 代表主页
@@ -28,11 +30,6 @@ elif st.session_state.current_page == 3:
 col1, col2 = st.columns(2)
 with col1:
     # 后退按钮，仅当当前页面不是主页时显示
-    if st.session_state.current_page > 0:
-        if st.button("后退"):
+    if st.session_state.current_page > 2:
+        if st.button("Back"):
             prev_page()
-with col2:
-    # 前进按钮，仅当当前页面不是最后一页时显示
-    if st.session_state.current_page < 3:
-        if st.button("前进"):
-            next_page()
